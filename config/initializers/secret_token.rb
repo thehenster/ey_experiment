2e66c9e8f6a84fd3adfd46a3a3d1ffa0747b4ed5
Henry Turner <henry.turner@unboxedconsulting.com>
2014-04-09 16:15:06 +0100

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EyExperiment::Application.config.secret_key_base = '94be400ee8698367be161ba431aeea2b49d8e4f31a774109496930d275bfd13b7648b6c6b31a2fe1ce2b467a9e80240bdb26a1759bd26aabbe5f22949e3831ce'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EyExperiment::Application.config.session_store :cookie_store, key: '_ey_experiment_session'
